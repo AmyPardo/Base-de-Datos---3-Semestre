edce302e8c0fb85d1d7b6ceb67c31141ef1f70f2
AmyPardo <am.pardoc@uea.edu.ec>
2026-01-11 16:57:47 -0500

    Deber Semana 5

---
+++ ESTRUCTURA DE DATOS/Ejercicios_Semana5/Ejercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Asignatura
+{
+    public string Nombre { get; set; }
+    public double Nota { get; set; }
+
+    public Asignatura(string nombre)
+    {
+        Nombre = nombre;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Asignatura> asignaturas = new List<Asignatura>
+        {
+            new Asignatura("Matemáticas"),
+            new Asignatura("Física"),
+            new Asignatura("Química"),
+            new Asignatura("Historia"),
+            new Asignatura("Lengua")
+        };
+
+        foreach (var a in asignaturas)
+        {
+            Console.Write($"Ingrese la nota de {a.Nombre}: ");
+            a.Nota = double.Parse(Console.ReadLine());
+        }
+
+        asignaturas.RemoveAll(a => a.Nota >= 7);
+
+        Console.WriteLine("\nAsignaturas que debe repetir:");
+        foreach (var a in asignaturas)
+        {
+            Console.WriteLine(a.Nombre);
+        }
+    }
+}
